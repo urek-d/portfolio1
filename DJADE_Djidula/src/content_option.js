2a92746fbef3a6a1b5fee0144486500183878ac0
urek-d <djidulad@gmail.com>
2023-04-20 01:52:42 +0000

    first commit

---
+++ DJADE_Djidula/src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "DJADE";
+const meta = {
+    title: "DJADE Kokou Djidula",
+    description: "Je suis DJADE kokou Didula  apprenti ingénieur en intelligence artificielle _ backend devloper,Actuellement en fin de licence",
+};
+
+const introdata = {
+    title: "JE me nome DJADE Djidula",
+    animated: {
+        first: "J'ADORE la programation et le deep learning",
+        second: " J'aime m'entrainer a creer des models de machine learning et creer des applications web",
+        third: "Je developpe aussi des apllications mobiles",
+    },
+    description: "Je suis un développeur polyvalent avec une expertise en développement de back-end utilisant Django, ainsi qu'en développement d'applications front-end avec React et React Native. Avec une solide connaissance des technologies web et mobile, je suis capable de concevoir et développer des applications performantes et fiables pour les entreprises et les utilisateurs finaux. J'aime travailler en équipe et je suis passionné par la résolution de problèmes complexes à l'aide de technologies innovantes",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A propos de moi",
+    aboutme: "je suis un développeur polyvalent avec une expertise en développement de back-end utilisant Django, ainsi qu'en développement d'applications front-end avec React et React Native. Avec une solide connaissance des technologies web et mobile, je suis capable de concevoir et développer des applications performantes et fiables pour les entreprises et les utilisateurs finaux. J'aime travailler en équipe et je suis passionné par la résolution de problèmes complexes à l'aide de technologies innovantes.",
+};
+const worktimeline = [{
+        jobtitle: "Developpeur frelance",
+        where: ".......",
+        date: "2023",
+    },
+    {
+        jobtitle: "Developpeur backend",
+        where: "ipnet (Projet professionnel etudiant)",
+        date: "2022",
+    },
+    {
+        jobtitle: "Developpeur backend",
+        where: ".....",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 75,
+    },
+    {
+        name: "Djano",
+        value: 70,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "react-native",
+        value: 60,
+    },
+    {
+        name: "Angular",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "dev backend",
+        description: "specialisé en developpement backend avec Django.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "je developpe aussi des application mobile native avec react-native et flutter.",
+    },
+    {
+        title: "ingenieur IA",
+        description: "En apprentissage.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "image projet 1.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Djidulad@gmail.com",
+    YOUR_FONE: "+22891022575",
+    description: ".............................. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_zorbzd9",
+    YOUR_TEMPLATE_ID: "template_ehq6rkz",
+    YOUR_USER_ID: "CyPjJjNaIBpQcuAWR",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
